d4383f96cad145db8a7e66f8888bc0f2dd3889d2
koushikgg <iamkoushikg@gmail.com>
2024-07-15 12:28:01 +0530

    [koushik]added: allbooks  added and book deatails added

---
+++ src/Components/AllBooks/AllBooks.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Book from "../Book/Book";
+import './AllBooks.scss';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+function AllBooks() {
+    const [booklist, setBooklist] = useState([]);
+    const [sortSelect, setSortSelect] = useState('');
+
+
+    return (
+        <>
+            <div className="allbooks-name-sort-opt-main-cnt">
+                <div className="allbooks-total-count-main-cnt">
+                    <p id="allbooks-book-text">Book</p>
+                    <p id="allbooks-total-count">(120 items)</p>
+                </div>
+                <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+                    <InputLabel id="demo-select-small-label">Sort by relevance  </InputLabel>
+                    <Select
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        label="Sort by relevance  "
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div className="allbooks-main-cnt">
+                <Book />
+            </div>
+        </>
+    );
+}
+
+export default AllBooks;
---
+++ src/Components/Book/Book.jsx
@@ -0,0 +1,29 @@
+import booklogo from '../../Assets/book1.png';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+import './Book.scss'
+
+function Book() {
+    return (
+        <>
+            <div className="book-main-cnt">
+                <div className="book-img-main-cnt">
+                    <img src={booklogo} alt="" />
+                </div>
+                <div className="book-details-main-cnt">
+                    <p id='book-details-bookname'>Don't Make Me Think</p>
+                    <p id='book-details-auther-name'>By Steve Krug</p>
+                    <div className="book-details-rating-count-cnt">
+                        <p id='book-details-bookrating'>4.5 <StarOutlinedIcon id='book-details-bookrating-star'/></p>
+                        <p id='book-details-bookcount'>(20)</p>
+                    </div>
+                    <div className="book-details-price-cnt">
+                        <p id='book-details-book-price'>Rs. 1500</p>
+                        <p id='book-details-book-actual-price'>Rs2000</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Book;
